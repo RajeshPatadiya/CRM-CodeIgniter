8177f17ac6b225cc3ba090597e25d4403a6a6652
mimarwa <bakloutimarwa80@yahoo.fr>
2015-07-16 00:54:39 +0100

    firsttry

---
+++ CodeIgniter-3.0.0/application/views/vue.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Document sans titre</title>
+</head>
+
+<body>
+<form name="form1" method="POST" >
+id: <input name="id_client" type="text" id="id_client" /> <br />
+Nom : <input name="nom" type="text" id="nom" /> <br />
+Prenom : <input name="prenom" type="text" id="prenom" /> <br />
+Adresse : <input name="adresse" type="text" id="adresse"  /> <br />
+Numero  : <input name="numero" type="text" id="tel"  /> <br />
+<input name="ajouter" type="submit" value="ajouter" id="submit" />
+<input name="annuler" type="reset" value="annuler" />
+</form>
+</body>
+</html>
